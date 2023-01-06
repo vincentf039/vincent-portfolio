4ef3d6d7a0da6fc5308399ae827070f8e49cd642
vincentf039 <vincentfernandez.ifl@gmail.com>
2023-01-06 16:16:34 +0800

    1st final done

--- script.js
+++ script.js
@@ -85,3 +85,20 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tabs");
 }
 
+//Google Sheets
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbydtiU9ufqN4IX09dbaZop6-OekSAyyrVJL4eFuua-ZM1tcd_H1pmyYtOJKOvlw7AkqCg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
